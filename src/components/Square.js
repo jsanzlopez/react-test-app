81a346dc2821645d418dc322b8359db891c0d729
jsanzlopez <jsanzlopez@mysidewalk.com>
2021-07-01 16:29:53 -0500

    Add bootstrap and new files

---
+++ src/components/Square.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/board.scss';
+
+export default class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
